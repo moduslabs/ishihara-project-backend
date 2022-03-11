f128462c2959ea12c1be0fc6145620fc88b581c8
Rafael Marangoni Waterkemper <rafael.waterkemper@betha.com.br>
2022-03-11 18:32:23 -0300

    Refactoring on the spotfactory;

--- plate-generator/algorithm/src/factory/spotFactory.js
+++ plate-generator/algorithm/src/factory/spotFactory.js
@@ -1,3 +1,7 @@
+const WHITE_COLOR_RGBA_SUM = 1020;
+const DEFAULT_ALPHA = 255;
+const RGBA_BYTES = 4;
+
 class SpotFactory {
 
     constructor(canvasWidth, canvasHeight) {
@@ -6,48 +10,50 @@ class SpotFactory {
     }
 
     create() {
-        var minRadius = (this.canvasWidth + this.canvasHeight) / 250;
-        var maxRadius = (this.canvasWidth + this.canvasHeight) / 100;
-        var radiusInternalCircle = minRadius + Math.random() * (maxRadius - minRadius);
+        const minRadius = (this.canvasWidth + this.canvasHeight) / 230;
+        const maxRadius = (this.canvasWidth + this.canvasHeight) / 100;
+        const radiusInternalCircle = minRadius + Math.random() * (maxRadius - minRadius);
 
-        var angle = Math.random() * 2 * Math.PI;
-        var radius = Math.random() * (Math.min(this.canvasWidth, this.canvasHeight) * 0.48 - 2);
-        var offsetX = this.canvasWidth * 0.5 + Math.cos(angle) * radius;
-        var offsetY = this.canvasHeight * 0.5 + Math.sin(angle) * radius;
+        const angle = Math.random() * 2 * Math.PI;
+        const radius = Math.random() * (Math.min(this.canvasWidth, this.canvasHeight) * 0.48 - 2);
+        const offsetX = this.canvasWidth * 0.5 + Math.cos(angle) * radius;
+        const offsetY = this.canvasHeight * 0.5 + Math.sin(angle) * radius;
 
         return { x: offsetX, y: offsetY, radius: radiusInternalCircle };
     }
 
-    isOverlapping(img_data, circle) {
-        var x = circle.x;
-        var y = circle.y;
-        var r = circle.radius;
+    isOverlapping(img_data, spot) {
+        const x = spot.x;
+        const y = spot.y;
+        const radius = spot.radius;
 
-        var dimensionsX = [x, x, x, x - r, x + r, x - r * 0.93, x - r * 0.93, x + r * 0.93, x + r * 0.93] //Calcula para ver se sobrepoem no eixo X
-        var dimensionsY = [y, y - r, y + r, y, y, y + r * 0.93, y - r * 0.93, y + r * 0.93, y - r * 0.93] //Calcula para ver se sobrepoem no eixo Y
+        const dimensionsX = [x, x, x, x - radius, x + radius, x - radius * 0.93, x - radius * 0.93, x + radius * 0.93, x + radius * 0.93] //Calcula para ver se sobrepoem no eixo X
+        const dimensionsY = [y, y - radius, y + radius, y, y, y + radius * 0.93, y - radius * 0.93, y + radius * 0.93, y - radius * 0.93] //Calcula para ver se sobrepoem no eixo Y
 
         for (var i = 0; i < dimensionsX.length; i++) {
-            var x = dimensionsX[i];
-            var y = dimensionsY[i];
+            const offsetX = dimensionsX[i];
+            const offsetY = dimensionsY[i];
 
-            var index = (Math.floor(y) * img_data.width + Math.floor(x)) * 4;
+            const index = (Math.floor(offsetY) * img_data.width + Math.floor(offsetX)) * RGBA_BYTES;
+            const red = img_data.data[index];
+            const green = img_data.data[index + 1];
+            const blue = img_data.data[index + 2];
 
-            var r = img_data.data[index];
-            var g = img_data.data[index + 1];
-            var b = img_data.data[index + 2];
-            var a = img_data.data[index + 3];
-
-            if ((r + g + b) * (a / 255) < 127) {
+            const pixelIsNotBlank = (red + green + blue + DEFAULT_ALPHA) !== WHITE_COLOR_RGBA_SUM;
+            if (pixelIsNotBlank) {
                 return true;
             }
         }
         return false;
     }
 
-    intersects(circle1, circle2) {
-        return Math.pow(circle2.x - circle1.x, 2) +
-            Math.pow(circle2.y - circle1.y, 2) <
-            Math.pow(circle1.radius + circle2.radius, 2);
+    intersects(newSpot, nearSpot) {
+        const axisSum = Math.pow(nearSpot.x - newSpot.x, 2) + Math.pow(nearSpot.y - newSpot.y, 2);
+        const radiusSum = Math.pow(newSpot.radius + nearSpot.radius, 2);
+        
+        const reductionOfDistanceBetweenSpots = 1.05;
+
+        return axisSum * reductionOfDistanceBetweenSpots < radiusSum;
     }
 
     draw(canvasCtx, circle) {
