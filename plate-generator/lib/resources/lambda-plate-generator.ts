2c2634608568a0bd839dbd16eb7f2209e6b5d341
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2022-04-25 23:16:44 -0300

    Add constants to memory size and timeout for lambda plate generator

--- plate-generator/lib/resources/lambda-plate-generator.ts
+++ plate-generator/lib/resources/lambda-plate-generator.ts
@@ -3,12 +3,15 @@ import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { getLogicalId, getNamespace } from "../util";
 
+const MEMORY_SIZE_MB = 180;
+const TIMEOUT_EXECUTION = Duration.minutes(5);
+
 export function getLambdaPlateGenerator(stack: Stack) {
   return new Function(stack, getLogicalId("lambda-plate-generator"), {
     runtime: Runtime.NODEJS_14_X,
     handler: "src/index.handler",
-    memorySize: 512,
-    timeout: Duration.minutes(10),
+    memorySize: MEMORY_SIZE_MB,
+    timeout: TIMEOUT_EXECUTION,
     code: Code.fromAsset(join(__dirname, "..", "..", "lambdas", "algorithm")),
     environment: {
       BUCKET_S3: `ishihara-${getNamespace()}`,
