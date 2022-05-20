06576cfd0c28a854185669a5771e1315e2c81994
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2022-05-20 14:47:47 -0300

    Add more time to get the response from getPlates api

--- lib/resources/lambda-api-get-plates.ts
+++ lib/resources/lambda-api-get-plates.ts
@@ -1,4 +1,4 @@
-import { Stack } from "aws-cdk-lib";
+import { Duration, Stack } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { getNamespace } from "../util";
@@ -6,6 +6,7 @@ import { getNamespace } from "../util";
 export function getLambdaSimpleApi(stack: Stack) {
   return new Function(stack, "lambda-get-plates", {
     runtime: Runtime.NODEJS_14_X,
+    timeout: Duration.seconds(30),
     handler: "src/index.handler",
     code: Code.fromAsset(join(__dirname, "..", "..", "lambdas", "api")),
     environment: {
