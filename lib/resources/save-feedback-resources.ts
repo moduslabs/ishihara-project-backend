7ce5c279f56e6bc915a8644ca5cb4d3d2d255c45
Hugo Zapata <hugozap@gmail.com>
2022-11-23 07:57:21 -0500

    Integrate S3 uploading logic (for feedback images)

--- lib/resources/save-feedback-resources.ts
+++ lib/resources/save-feedback-resources.ts
@@ -1,18 +1,12 @@
 /* create cdk resources for feedback feature (lambda and dynamodb table) */
 
 import { Table, TableProps } from "aws-cdk-lib/aws-dynamodb";
-import {
-  Function,
-  FunctionProps,
-  Runtime,
-  Code,
-} from "aws-cdk-lib/aws-lambda";
+import { Function, FunctionProps, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { getNamespace } from "../util";
 import { AttributeType } from "aws-cdk-lib/aws-dynamodb";
 
 import { Bucket, BucketAccessControl, HttpMethods } from "aws-cdk-lib/aws-s3";
-
 
 export function getFeedbackTable(scope: Construct, props?: TableProps): Table {
   return new Table(scope, `FeedbackTable${getNamespace()}`, {
@@ -26,7 +20,7 @@ export function getFeedbackTable(scope: Construct, props?: TableProps): Table {
 
 export function getLambdaApiSaveFeedback(
   scope: Construct,
-  //optional 
+  //optional
   props?: Partial<FunctionProps>
 ): Function {
   return new Function(scope, `LambdaApiSaveFeedback${getNamespace()}`, {
@@ -34,5 +28,30 @@ export function getLambdaApiSaveFeedback(
     handler: "saveFeedback.handler",
     code: Code.fromAsset("lambdas/feedback"),
     ...props,
+  });
+}
+
+export function getUploadImageLambda(
+  scope: Construct,
+  props?: Partial<FunctionProps>
+): Function {
+  return new Function(scope, `LambdaUploadImage${getNamespace()}`, {
+    runtime: Runtime.NODEJS_14_X,
+    handler: "uploadImage.handler",
+    code: Code.fromAsset("lambdas/getUploadImageSignedUrl"),
+    ...props,
+  });
+}
+
+export function getFeedbackImageBucket(scope: Construct): Bucket {
+  return new Bucket(scope, `FeedbackImageBucket${getNamespace()}`, {
+    accessControl: BucketAccessControl.PRIVATE,
+    cors: [
+      {
+        allowedOrigins: ["*"],
+        allowedMethods: [HttpMethods.GET, HttpMethods.PUT, HttpMethods.HEAD],
+      },
+    ],
+    bucketName: `feedback-image-${getNamespace()}`,
   });
 }
